17bdd8f32ccc37d897125e0abfdb7cc47884db9c
GFLEMING133 <32335233+GFLEMING133@users.noreply.github.com>
2017-12-04 11:08:41 -0600

    Cleanup
    Changes of a few more acces modifiers (private instead of public) and added MSOE coding standard's checkStyle element

--- fx/src/Scenes/VideoController.java
+++ fx/src/Scenes/VideoController.java
@@ -8,8 +8,6 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.control.Button;
-import javafx.geometry.Rectangle2D;
-import javafx.util.Duration;
 
 import java.nio.file.Paths;
 import java.util.Timer;
@@ -46,8 +44,7 @@ public class VideoController {
             }
         });
     }
-
-    public void plausePlay() {
+    private void plausePlay() {
         if (playing) {
             player.pause();
             playing = false;
@@ -62,12 +59,12 @@ public class VideoController {
         }
     }
 
-    public void callToQuiz() {
+    private void callToQuiz() {
         System.out.println("No problems with pressing button...");
         mt.moveToQuizScene();
     }
 
-    public void checkTime(double timeSec)
+    private void checkTime(double timeSec)
     {
         time = new MyTimerClass();
         time.getObject(this);
